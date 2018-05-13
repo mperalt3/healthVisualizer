0e901a6757a3fef5cae7d06e0a53a7f98a0edf82
mperalta_costoya <mperalta.costoya@gmail.com>
2018-05-13 00:22:10 -0400

    Load Diabetes prevelance by gender

--- api/dist/scripts/populateDb.js
+++ api/dist/scripts/populateDb.js
@@ -35,71 +35,62 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 console.log('This script populates database');
 
 
+// Open database conection
 var mongoDB = 'mongodb://127.0.0.1/' + _db.databaseConfiguration.name;
-
 _mongoose2.default.connect(mongoDB);
 _mongoose2.default.Promise = global.Promise;
 var db = _mongoose2.default.connection;
 _mongoose2.default.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-var diseases = [];
-var states = [];
-
+// This function clear all documents from the database collections
 async function clearModels() {
-  console.log("removing existing data");
+  console.log("removing existing data from");
   await _county2.default.remove({}, function (err, result) {
     if (err) {
       console.log(err);
     }
-    console.log(result);
+    console.log("Counties");
   }).exec();
   await _disease2.default.remove({}, function (err, result) {
     if (err) {
       console.log(err);
     }
-    console.log(result);
+    console.log("Diseases");
   }).exec();
   await _state2.default.remove({}, function (err, result) {
     if (err) {
       console.log(err);
     }
-    console.log(result);
+    console.log("States");
   }).exec();
   await _statistic2.default.remove({}, function (err, result) {
     if (err) {
       console.log(err);
     }
-    console.log(result);
+    console.log("Statistics");
   }).exec();
-  console.log("cleared!");
+  console.log("Data cleared!");
   return;
 }
 
-function returnResult(err, result) {
-  if (err) {
-    console.log("error" + err);
-  }
-  console.log("resultado:" + result);
-  return result;
-}
-
+// This function load data from DM_PREV_ALL_STATES.xlsx file
 async function loadDiabetesPrevalence() {
-  console.log("loadDiabetesPrevalence");
+  console.log("load Diabetes Prevalence data");
   var workbook = _xlsx2.default.readFile('dist/data_source/DM_PREV_ALL_STATES.xlsx');
   var fullDocument = _xlsx2.default.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]], { header: 1 });
-  var disease = await _disease2.default.findOneOrCreate({ name: "Obesity" });
+  var disease = await _disease2.default.findOneOrCreate({ name: "Diabetes" });
   var years = fullDocument[0].slice(1).filter(function (cell) {
     return cell != "";
   });
   var row = 2;
   var column = 0;
   while (row < fullDocument.length) {
-    var state = await _state2.default.findOneOrCreate({ name: fullDocument[row][column] }, returnResult);
+    var state = await _state2.default.findOneOrCreate({ name: fullDocument[row][column] });
     var county = await _county2.default.findOneOrCreate({
       fipsCode: fullDocument[row][column + 1],
       name: fullDocument[row][column + 2],
       stateId: state._id
-    }, returnResult);
+    });
     column = column + 3;
     var year = 0;
     while (column < fullDocument[row].length) {
@@ -131,21 +122,73 @@ async function loadDiabetesPrevalence() {
 }
 
 async function loadDiabetesPrevalenceByGender() {
-  console.log("loadDiabetesPrevalenceByGender");
-  var state = await new _state2.default({ name: "Florida" });
-  await state.save(function (err) {
-    if (err) {
-      console.log('ERROR CREATING state ' + state);
-      console.log(err);
-      return;
-    }
-    console.log('New state: ' + state);
+  console.log("load Diabetes Prevalence By Gender data");
+  var workbook = _xlsx2.default.readFile('dist/data_source/DM_PREV_by_sex_ALL_STATES.xlsx');
+  var fullDocument = _xlsx2.default.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]], { header: 1 });
+  var disease = await _disease2.default.findOneOrCreate({ name: "Diabetes" });
+  var years = fullDocument[0].slice(1).filter(function (cell) {
+    return cell != "";
   });
-  states.push(state);
+  console.log(years);
+  var row = 2;
+  var column = 0;
+  while (row < fullDocument.length) {
+    var state = await _state2.default.findOneOrCreate({ name: fullDocument[row][column] });
+    var county = await _county2.default.findOneOrCreate({
+      fipsCode: fullDocument[row][column + 1],
+      name: fullDocument[row][column + 2],
+      stateId: state._id
+    });
+    column = column + 3;
+    var year = 0;
+    while (column < fullDocument[row].length) {
+      var statsMale = new _statistic2.default({
+        countyId: county._id,
+        diseaseId: disease._id,
+        statisticDate: new Date(years[year]),
+        totalCount: fullDocument[row][column] != "No Data" ? fullDocument[row][column] : null,
+        percent: fullDocument[row][column + 1] != "No Data" ? fullDocument[row][column + 1] : null,
+        lowerConfidenceLimit: fullDocument[row][column + 2] != "No Data" ? fullDocument[row][column + 2] : null,
+        upperConfidenceLimit: fullDocument[row][column + 3] != "No Data" ? fullDocument[row][column + 3] : null,
+        ageAdjustedPercent: fullDocument[row][column + 4] != "No Data" ? fullDocument[row][column + 4] : null,
+        ageLowerConfidenceLimit: fullDocument[row][column + 5] != "No Data" ? fullDocument[row][column + 5] : null,
+        ageUpperConfidenceLimit: fullDocument[row][column + 6] != "No Data" ? fullDocument[row][column + 6] : null,
+        genderScope: "M"
+      });
+      var statsFemale = new _statistic2.default({
+        countyId: county._id,
+        diseaseId: disease._id,
+        statisticDate: new Date(years[year]),
+        totalCount: fullDocument[row][column + 7] != "No Data" ? fullDocument[row][column + 7] : null,
+        percent: fullDocument[row][column + 8] != "No Data" ? fullDocument[row][column + 8] : null,
+        lowerConfidenceLimit: fullDocument[row][column + 9] != "No Data" ? fullDocument[row][column + 9] : null,
+        upperConfidenceLimit: fullDocument[row][column + 10] != "No Data" ? fullDocument[row][column + 10] : null,
+        ageAdjustedPercent: fullDocument[row][column + 11] != "No Data" ? fullDocument[row][column + 11] : null,
+        ageLowerConfidenceLimit: fullDocument[row][column + 12] != "No Data" ? fullDocument[row][column + 12] : null,
+        ageUpperConfidenceLimit: fullDocument[row][column + 13] != "No Data" ? fullDocument[row][column + 13] : null,
+        genderScope: "F"
+      });
+      statsMale.save(function (err, result) {
+        if (err) {
+          console.log(err);
+        }
+      });
+      statsFemale.save(function (err, result) {
+        if (err) {
+          console.log(err);
+        }
+      });
+      column = column + 14;
+      year++;
+    }
+    column = 0;
+    row++;
+  }
   return;
 }
 
-_async2.default.series([clearModels, loadDiabetesPrevalence], function (err, results) {
+// This function call the following functions in order to populate the database
+_async2.default.series([clearModels, loadDiabetesPrevalence, loadDiabetesPrevalenceByGender], function (err, results) {
   if (err) {
     console.log('FINAL ERR: ' + err);
   } else {
--- api/scripts/populateDb.js
+++ api/scripts/populateDb.js
@@ -8,59 +8,52 @@ import Disease from '../app/models/disease';
 import State from '../app/models/state';
 import Statistic from '../app/models/statistic';
 
+// Open database conection
 const mongoDB = 'mongodb://127.0.0.1/' + databaseConfiguration.name;
-
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
 let db = mongoose.connection;
 mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-let diseases = [];
-let states = [];
-
+// This function clear all documents from the database collections
 async function clearModels(){
-  console.log("removing existing data")
+  console.log("removing existing data from")
   await County.remove({}, (err, result)=>{
     if(err){ console.log(err) }
-    console.log(result);
+    console.log("Counties");
   }).exec();
   await Disease.remove({}, (err, result)=>{
     if(err){ console.log(err) }
-    console.log(result);
+    console.log("Diseases");
   }).exec();
   await State.remove({}, (err, result)=>{
     if(err){ console.log(err) }
-    console.log(result);
+    console.log("States");
   }).exec();
   await Statistic.remove({}, (err, result)=>{
     if(err){ console.log(err) }
-    console.log(result);
+    console.log("Statistics");
   }).exec();
-  console.log("cleared!")
+  console.log("Data cleared!")
   return;
 }
 
-function returnResult(err, result){
-  if (err){ console.log("error"+err) }
-  console.log("resultado:"+result)
-  return result;
-}
-
+// This function load data from DM_PREV_ALL_STATES.xlsx file
 async function loadDiabetesPrevalence(){
-  console.log("loadDiabetesPrevalence");
+  console.log("load Diabetes Prevalence data");
   const workbook = XLSX.readFile('dist/data_source/DM_PREV_ALL_STATES.xlsx');
-  const fullDocument = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]], {header:1});
-  let disease = await Disease.findOneOrCreate({ name: "Obesity" });
+  const fullDocument = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]], { header: 1 });
+  let disease = await Disease.findOneOrCreate({ name: "Diabetes" });
   let years = fullDocument[0].slice(1).filter(cell => cell != "");
   let row = 2;
   let column = 0;
   while(row < fullDocument.length){
-    let state = await State.findOneOrCreate({ name: fullDocument[row][column] }, returnResult);
+    let state = await State.findOneOrCreate({ name: fullDocument[row][column] });
     let county = await County.findOneOrCreate({
       fipsCode: fullDocument[row][column+1],
       name: fullDocument[row][column+2],
       stateId: state._id
-    }, returnResult);
+    });
     column = column+3;
     let year = 0;
     while(column < fullDocument[row].length){
@@ -91,24 +84,70 @@ async function loadDiabetesPrevalence(){
 
 
 async function loadDiabetesPrevalenceByGender(){
-  console.log("loadDiabetesPrevalenceByGender")
-  let state = await new State({name: "Florida"});
-  await state.save(function (err) {
-    if (err) {
-      console.log('ERROR CREATING state ' + state);
-      console.log(err)
-      return
+  console.log("load Diabetes Prevalence By Gender data");
+  const workbook = XLSX.readFile('dist/data_source/DM_PREV_by_sex_ALL_STATES.xlsx');
+  const fullDocument = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]], { header: 1 });
+  let disease = await Disease.findOneOrCreate({ name: "Diabetes" });
+  let years = fullDocument[0].slice(1).filter(cell => cell != "");
+  console.log(years)
+  let row = 2;
+  let column = 0;
+  while(row < fullDocument.length){
+    let state = await State.findOneOrCreate({ name: fullDocument[row][column] });
+    let county = await County.findOneOrCreate({
+      fipsCode: fullDocument[row][column+1],
+      name:     fullDocument[row][column+2],
+      stateId:  state._id
+    });
+    column = column + 3;
+    let year = 0;
+    while(column < fullDocument[row].length){
+      let statsMale = new Statistic({
+        countyId:       county._id,
+        diseaseId:      disease._id,
+        statisticDate:  new Date(years[year]),
+        totalCount:               fullDocument[row][column] != "No Data" ? fullDocument[row][column] : null,
+        percent:                  fullDocument[row][column + 1] != "No Data" ? fullDocument[row][column + 1] : null,
+        lowerConfidenceLimit:     fullDocument[row][column + 2] != "No Data" ? fullDocument[row][column + 2] : null,
+        upperConfidenceLimit:     fullDocument[row][column + 3] != "No Data" ? fullDocument[row][column + 3] : null,
+        ageAdjustedPercent:       fullDocument[row][column + 4] != "No Data" ? fullDocument[row][column + 4] : null,
+        ageLowerConfidenceLimit:  fullDocument[row][column + 5] != "No Data" ? fullDocument[row][column + 5] : null,
+        ageUpperConfidenceLimit:  fullDocument[row][column + 6] != "No Data" ? fullDocument[row][column + 6] : null,
+        genderScope: "M"
+      });
+      let statsFemale = new Statistic({
+        countyId:       county._id,
+        diseaseId:      disease._id,
+        statisticDate:  new Date(years[year]),
+        totalCount:               fullDocument[row][column + 7] != "No Data" ? fullDocument[row][column + 7] : null,
+        percent:                  fullDocument[row][column + 8] != "No Data" ? fullDocument[row][column + 8] : null,
+        lowerConfidenceLimit:     fullDocument[row][column + 9] != "No Data" ? fullDocument[row][column + 9] : null,
+        upperConfidenceLimit:     fullDocument[row][column + 10] != "No Data" ? fullDocument[row][column + 10] : null,
+        ageAdjustedPercent:       fullDocument[row][column + 11] != "No Data" ? fullDocument[row][column + 11] : null,
+        ageLowerConfidenceLimit:  fullDocument[row][column + 12] != "No Data" ? fullDocument[row][column + 12] : null,
+        ageUpperConfidenceLimit:  fullDocument[row][column + 13] != "No Data" ? fullDocument[row][column + 13] : null,
+        genderScope: "F"
+      });
+      statsMale.save((err, result)=>{
+        if (err){ console.log(err) }
+      });
+      statsFemale.save((err, result)=>{
+        if (err){ console.log(err) }
+      });
+      column = column + 14;
+      year++;
     }
-    console.log('New state: ' + state);
-  });
-  states.push(state);
+    column=0;
+    row++;
+  }
   return;
 }
 
+// This function call the following functions in order to populate the database
 async.series([
     clearModels,
     loadDiabetesPrevalence,
-    // loadDiabetesPrevalenceByGender
+    loadDiabetesPrevalenceByGender
 ],
 (err, results) => {
     if (err) {
