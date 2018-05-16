67e39cbb44ef24203f3a55b8983bee711c078069
mperalta_costoya <mperalta.costoya@gmail.com>
2018-05-16 18:02:53 -0400

    Add react-dom router and basic estructure for redux

---
+++ health-visualizer-frontend/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from '../assets/images/logo.svg';
+import '../assets/stylesheets/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <h2>Hello world</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
--- health-visualizer-frontend/src/index.js
+++ health-visualizer-frontend/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './assets/stylesheets/index.css';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+window.store = store;
+window.addArticle = addArticle;
+
+ReactDOM.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  , document.getElementById('root'));
 registerServiceWorker();
