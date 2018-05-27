2a8db92ee911c10f27af85e897497ae8566bbc81
mperalta_costoya <mperalta.costoya@gmail.com>
2018-05-27 12:43:25 -0400

    Add scroll to landing page

--- health-visualizer-frontend/src/components/landing.js
+++ health-visualizer-frontend/src/components/landing.js
@@ -1,12 +1,49 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import FontAwesome from 'react-fontawesome';
-
+import { DirectLink, Element , Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
 
 class Landing extends Component {
   constructor(){
     super();
     this.state = {};
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentDidMount(){
+    Events.scrollEvent.register('begin', function() {});
+    Events.scrollEvent.register('end', function() {});
+  }
+
+  componentWillUnmount() {
+    Events.scrollEvent.remove('begin');
+    Events.scrollEvent.remove('end');
+  }
+
+  handleClick(event){
+    event.preventDefault();
+    if (event.target.id == "learn more"){
+      this.scrollToAbout();
+    }else{
+
+    }
+
+  }
+
+  scrollToTop() {
+    scroll.scrollToTop({
+      smooth: "easeInQuads",
+      delay: 80
+    });
+  }
+
+  scrollToAbout(){
+    scroller.scrollTo('About', {
+      duration: 800,
+      delay: 0,
+      smooth: 'easeInOutQuart',
+      offset: -120
+    });
   }
 
   render(){
@@ -16,11 +53,12 @@ class Landing extends Component {
           <h1> Welcome to Health Visualizer </h1>
           <p> This site display information about indicators that affect metabolic syndrome </p>
           <div>
-            <a href="" className="btn btn-info btn-lg landing-link" >Get starting</a>
-            <a href="" className="btn btn-info btn-lg landing-link" >Learn more</a>
+            <a href="" className="btn btn-info btn-lg landing-link" id="getting started" onClick={this.handleClick} >Getting started</a>
+            <a href="" className="btn btn-info btn-lg landing-link" id="learn more" onClick={this.handleClick}>Learn more</a>
           </div>
         </div>
         <div className="landing-section" id="about">
+          <Element name="About"></Element>
           <h2> <FontAwesome
             className='fas fa-heartbeat'
             name='heartbeat'
